b940a0930429aa9db4a46e3fab5b78232184e715
Vknight <jiankun.wan@padmatek.com>
2016-09-30 02:38:53 +0800

    增加示例程序

---
+++ VKFileBrowserDemo/VKFileBrowserDemo/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  VKFileBrowserDemo
+//
+//  Created by Vk on 2016/9/30.
+//  Copyright © 2016年 vk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let vc = VKFileViewController()
+        
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
